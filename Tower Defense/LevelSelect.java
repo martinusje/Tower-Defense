a6018368b3b46be280b6498ec441885d89fc2f54
SecretAgentJoost <jvanderweerd@unic-utrecht.nl>
2016-01-29 15:27:54 +0100

    lvl_Selectbutton

--- Tower Defense/LevelSelect.java
+++ Tower Defense/LevelSelect.java
@@ -25,21 +25,29 @@ public class LevelSelect extends World
         addObject(new Text("Tower Defense!", 50), 384, 200);
         
         //Level clicker
-        addObject(new button("Level 1"), 300, 384);
+        addObject(new button("Level 1", 1), 300, 384);
         getBackground().drawImage(new GreenfootImage("Track_1_lvlselect.png"), 234, 400);
-        addObject(new button("Level 2"), 460, 384);
+        addObject(new button("Level 2", 1), 460, 384);
         getBackground().drawImage(new GreenfootImage("Track_2_lvlselect.png"), 396, 400);
-        
+        addObject(new button("", 2), 298, 463);
+        addObject(new button("", 2), 460, 463);
         //Start without clicking start
         Greenfoot.start();
     }
     
     public void act() {
-        //If clicked one of the levels
+        //If clicked one of the levels text button
         if(Greenfoot.mouseClicked(getObjects(button.class).get(0)) || Greenfoot.mouseClicked(getObjects(button.class).get(0))|| Greenfoot.isKeyDown("1")) {
             Greenfoot.setWorld(new Level(1));
         }
         if(Greenfoot.mouseClicked(getObjects(button.class).get(1)) || Greenfoot.isKeyDown("2")) {
+            Greenfoot.setWorld(new Level(2));
+        }
+        //If clicked one of the levels image button
+        if(Greenfoot.mouseClicked(getObjects(button.class).get(2)) || Greenfoot.isKeyDown("2")) {
+            Greenfoot.setWorld(new Level(1));
+        }
+        if(Greenfoot.mouseClicked(getObjects(button.class).get(3)) || Greenfoot.isKeyDown("2")) {
             Greenfoot.setWorld(new Level(2));
         }
     }
