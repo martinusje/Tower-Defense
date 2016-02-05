d1638bbcaeb8fe561410ff07227632b9b260e4d2
martinusje <martijn264@gmail.com>
2016-02-05 14:40:35 +0100

    Button gecomment

--- Tower Defense/button.java
+++ Tower Defense/button.java
@@ -13,24 +13,36 @@ public class button extends Actor
      * Act - do whatever the button wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    int buttonTypeJWZ;
+    
+    //Buttontype, 1 == with text, 2 == with image
+    int buttonType;
     public button(String text, int buttonType, int blackorwhite)
     {
-        buttonTypeJWZ = buttonType;
+        //Set buttontype from parameter
+        this.buttonType = buttonType;
+        
+        //Run updateimage method, comes from updatable image types (coins, etc.)
         updateImage(text, blackorwhite);
     }
  
     public void updateImage(String text, int blackorwhite)
     {
-        if (buttonTypeJWZ == 1){
+        //If text button
+        if (buttonType == 1){
+            //If black text
             if(blackorwhite == 0) {
+                //Set text
                 setImage(new GreenfootImage(text, 26, Color.black, new Color(0, 0, 0, 0)));
             }
+            //If white text
             if(blackorwhite == 1) {
+                //Set text
                 setImage(new GreenfootImage(text, 26, Color.white, new Color(0, 0, 0, 0)));
             }
         }
-        if (buttonTypeJWZ == 2){
+        //If imagebutton
+        if (buttonType == 2){
+            //Get emtpy button, image is set in method caller
             setImage(new GreenfootImage("Empty_Button.png"));
         }
     }       
